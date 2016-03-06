6fee2ebaad5ef889864896c3098599b8ecd420a6
Val Booth <vxb4825@rit.edu>
2016-03-05 19:55:43 -0500

    Trying to integrate plamere's SortYourMusic.

--- DjangoWebProject/urls.py
+++ DjangoWebProject/urls.py
@@ -16,6 +16,7 @@ urlpatterns = patterns('',
     url(r'^$', 'app.views.home', name='home'),
     url(r'^contact$', 'app.views.contact', name='contact'),
     url(r'^about', 'app.views.about', name='about'),
+    url(r'^test', 'app.views.test', name='test'),
     url(r'^login/$',
         'django.contrib.auth.views.login',
         {
