7e9e4766537c517694a2bd235dac5f156943a8bb
Val Booth <vxb4825@rit.edu>
2016-03-06 00:12:12 -0500

    I CAN GET THE PLAYLIST TABLES TO FILL WITH ECHONEST

--- app/static/app/scripts/config.js
+++ app/static/app/scripts/config.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var SPOTIFY_CLIENT_ID = '31469b011d4941bf8dd4ac9cf8495bac';
-var SPOTIFY_REDIRECT_URI = 'http://localhost:8235/SortYourMusic/';
-var SPOTIFY_REDIRECT_URI = 'http://localhost:8000/';
-var SPOTIFY_REDIRECT_URI = 'http://static.echonest.com/SortYourMusic/';
+var SPOTIFY_CLIENT_ID = '3460a309c5094c97bba78e98b5d6b90f';
+//var SPOTIFY_REDIRECT_URI = 'http://localhost:8235/SortYourMusic/';
+var SPOTIFY_REDIRECT_URI = 'http://localhost:8000/test';
+//var SPOTIFY_REDIRECT_URI = 'http://static.echonest.com/SortYourMusic/';
