edda16a6c1ecc3cdf5bcac7150a45fc50e2c2c1d
Val Booth <vxb4825@rit.edu>
2016-03-12 20:52:28 -0500

    Correct SPOTIFY_REDIRECT_URL.

--- app/static/app/scripts/config.js
+++ app/static/app/scripts/config.js
@@ -1,6 +1,6 @@
 "use strict";
 
 var SPOTIFY_CLIENT_ID = '3460a309c5094c97bba78e98b5d6b90f';
-//var SPOTIFY_REDIRECT_URI = 'http://www.mezclador.me/';
+var SPOTIFY_REDIRECT_URI = 'http://mezcladorme.azurewebsites.net/';
 //For local development
-var SPOTIFY_REDIRECT_URI = 'http://localhost:8000/';
+//var SPOTIFY_REDIRECT_URI = 'http://localhost:8000/';
