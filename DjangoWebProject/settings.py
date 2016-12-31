52060d22321f66ea66fd5967bb6adb7c2a7a248b
Val Booth <vxb4825@rit.edu>
2016-12-30 20:50:18 -0700

    Turning DEBUG=FALSE broke things, trying to add logging to figure out why.

--- DjangoWebProject/settings.py
+++ DjangoWebProject/settings.py
@@ -144,6 +144,7 @@ INSTALLED_APPS = (
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -153,10 +154,9 @@ LOGGING = {
         }
     },
     'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
+         'logfile': {
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': 'D:\home\site\wwwroot\myapp.log'
         }
     },
     'loggers': {
