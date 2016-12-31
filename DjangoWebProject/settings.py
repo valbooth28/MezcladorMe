2263d290f6ef04a10d32f3191597e2f786982885
Val Booth <vxb4825@rit.edu>
2016-12-30 21:27:29 -0700

    Reverted logging since it wasn't working and causing problems locally. Trying to fix DiagnosticTraceListenerIssue.

--- DjangoWebProject/settings.py
+++ DjangoWebProject/settings.py
@@ -144,7 +144,6 @@ INSTALLED_APPS = (
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
-
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -154,9 +153,10 @@ LOGGING = {
         }
     },
     'handlers': {
-         'logfile': {
-            'class': 'logging.handlers.WatchedFileHandler',
-            'filename': 'D:\home\site\wwwroot\myapp.log'
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
         }
     },
     'loggers': {
